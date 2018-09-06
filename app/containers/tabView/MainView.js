4c526929dd9a2693d7f1aac1a73b039b0f9cc67a
IoriCnn <chengeng0502@126.com>
2018-09-06 17:48:46 +0800

    完成动态读取导航功能

--- app/containers/tabView/MainView.js
+++ app/containers/tabView/MainView.js
@@ -10,101 +10,92 @@ import Statistical from './Statistical';
 import Account from './Account';
 import ConsumableManage from '../workbench/ConsumableManage';
 
-import {loadStorage} from '../../dvapack/storage';
+import { loadStorage } from '../../dvapack/storage';
 
 // create a component
 class Main extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      loading:true,
-      aa:{},
+      loading: true,
+      aa: {},
     };
   }
 
-  async componentWillMount(){
-    if(this.state.loading == true) {
-      let nav={
+  async componentWillMount() {
+    if (this.state.loading == true) {
+      let nav = {
         // Map: { screen: Map },
       };
-      const navs = await loadStorage("mainnavs");
+      const navs = await loadStorage('mainnavs');
       for (const r of navs) {
         for (const e of r.children) {
-            if (r.text === "环保专工") {
-                if(e.text === "数据"){
-                  nav.DataList= { screen: DataList };
-                }else if(e.text === "报警"){
-                  
-                }else if(e.text === "报备"){
-                  
-                }else if(e.text === "分析"){
-                  nav.Statistical= { screen: Statistical };
-                }else if(e.text === "我的"){
-                  nav.Account= { screen: Account };
-                }
-            }else if (r.text === "监测专员") {
-                if(e.text === "数据"){
-                  nav.DataList= { screen: DataList };
-                }else if(e.text === "报警"){
-                  
-                }else if(e.text === "工作台"){
-                  nav.Workbench={ screen: Workbench }; 
-                }else if(e.text === "记录"){
-                  
-                }else if(e.text === "我的"){
-                  nav.Account= { screen: Account };
-                }
-            }else if (r.text === "运维主管") {
-                if(e.text === "工作台"){
-                  nav.Workbench={ screen: Workbench }; 
-                }else if(e.text === "数据"){
-                  nav.DataList= { screen: DataList };
-                }else if(e.text === "历史"){
-                  
-                }else if(e.text === "分析"){
-                  nav.Statistical= { screen: Statistical };
-                }else if(e.text === "我的"){
-                  nav.Account= { screen: Account };
-                }
-            }else if (r.text === "运维人员") {
-                if(e.text === "工作台"){
-                  nav.Workbench={ screen: Workbench };
-                }else if(e.text === "数据一览"){
-                  nav.DataList= { screen: DataList };
-                }else if(e.text === "历史记录"){
-                  
-                }else if(e.text === "我的"){
-                  nav.Account= { screen: Account };
-                }
-           }
+          if (r.text === '环保专工') {
+            if (e.text === '数据') {
+              nav.DataList = { screen: DataList };
+            } else if (e.text === '报警') {
+            } else if (e.text === '报备') {
+            } else if (e.text === '分析') {
+              nav.Statistical = { screen: Statistical };
+            } else if (e.text === '我的') {
+              nav.Account = { screen: Account };
+            }
+          } else if (r.text === '监测专员') {
+            if (e.text === '数据') {
+              nav.DataList = { screen: DataList };
+            } else if (e.text === '报警') {
+            } else if (e.text === '工作台') {
+              nav.Workbench = { screen: Workbench };
+            } else if (e.text === '记录') {
+            } else if (e.text === '我的') {
+              nav.Account = { screen: Account };
+            }
+          } else if (r.text === '运维主管') {
+            if (e.text === '工作台') {
+              nav.Workbench = { screen: Workbench };
+            } else if (e.text === '数据') {
+              nav.DataList = { screen: DataList };
+            } else if (e.text === '历史') {
+            } else if (e.text === '分析') {
+              nav.Statistical = { screen: Statistical };
+            } else if (e.text === '我的') {
+              nav.Account = { screen: Account };
+            }
+          } else if (r.text === '运维人员') {
+            if (e.text === '工作台') {
+              nav.Workbench = { screen: Workbench };
+            } else if (e.text === '数据一览') {
+              nav.DataList = { screen: DataList };
+            } else if (e.text === '历史记录') {
+            } else if (e.text === '我的') {
+              nav.Account = { screen: Account };
+            }
+          }
         }
-    }
+      }
       debugger;
-     
+
       this.setState({
-        loading:false,
-        aa:TabNavigator(nav,
-          {
-            headerMode: 'none',
-            tabBarComponent: TabBarBottom,
-            tabBarPosition: 'bottom',
-            swipeEnabled: false,
-            animationEnabled: false,
-            lazyLoad: false,
-          })        
+        loading: false,
+        aa: TabNavigator(nav, {
+          headerMode: 'none',
+          tabBarComponent: TabBarBottom,
+          tabBarPosition: 'bottom',
+          swipeEnabled: false,
+          animationEnabled: false,
+          lazyLoad: false,
+        }),
       });
     }
   }
- 
+
   render() {
-      if (this.state.loading) {
-        return <Text>123</Text>;
-      } else {
-        const SimpleAppNavigator = this.state.aa;
-        return(
-          <SimpleAppNavigator />
-      );
-    }      
+    if (this.state.loading) {
+      return <Text>123</Text>;
+    } else {
+      const SimpleAppNavigator = this.state.aa;
+      return <SimpleAppNavigator />;
+    }
   }
 }
 
--- app/models/app.js
+++ app/models/app.js
@@ -6,9 +6,9 @@ import {
   saveToken,
   saveStorage,
   loadStorage,
-  storageload
+  storageload,
 } from '../dvapack/storage';
-import {getNavigators} from '../services/auth';
+import { getNavigators } from '../services/auth';
 
 export default Model.extend({
   namespace: 'app',
@@ -53,21 +53,19 @@ export default Model.extend({
       let workbenchs;
       let stations;
       const result = yield call(getNavigators);
-      if(result.data.length > 0){
-        const rdata=result.data;
-        mainnavs = rdata.find((e) => e.text === "主导航");
-        workbenchs = rdata.find((e) => e.text === "工作台");
-        stations = rdata.find((e) => e.text === "排口信息");
+      if (result.data.length > 0) {
+        const rdata = result.data;
+        mainnavs = rdata.find(e => e.text === '主导航');
+        workbenchs = rdata.find(e => e.text === '工作台');
+        stations = rdata.find(e => e.text === '排口信息');
       }
-      
 
-      yield saveStorage("mainnavs",mainnavs.children);
-      yield saveStorage("workbenchs",mainnavs.children);
-      yield saveStorage("stations",mainnavs.children);
+      yield saveStorage('mainnavs', mainnavs.children);
+      yield saveStorage('workbenchs', mainnavs.children);
+      yield saveStorage('stations', mainnavs.children);
 
-
-      const abd=yield storageload("mainnavs");
-      const ddd=yield loadStorage("mainnavs");
+      const abd = yield storageload('mainnavs');
+      const ddd = yield loadStorage('mainnavs');
       /*
     branchOffice: { screen: branchOffice },
     operationsStaff: { screen: operationsStaff },
